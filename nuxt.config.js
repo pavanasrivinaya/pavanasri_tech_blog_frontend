cff92ad83fd59c7613a39580d821abf822b48175
pavana sri <paripalepu@gmail.com>
2021-07-02 21:51:13 +0530

    second

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,4 @@
-const URL = 'http://localhost:3000'
+const URL = 'https://pavanasritechblogbackend.herokuapp.com'
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
